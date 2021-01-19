1659578a0b1870cb30adca6f881b6ae52e1b20ff
doYouSpeakFish <ed_sills@hotmail.co.uk>
2021-01-19 15:54:15 +0000

    Generated builder module and stat-form component

---
+++ quick-monster/src/app/builder/builder.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StatsFormComponent } from './stats-form/stats-form.component';
+
+
+
+@NgModule({
+  declarations: [StatsFormComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BuilderModule { }
---
+++ quick-monster/src/app/builder/stats-form/stats-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stats-form',
+  templateUrl: './stats-form.component.html',
+  styleUrls: ['./stats-form.component.css']
+})
+export class StatsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
