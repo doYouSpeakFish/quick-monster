d9fa95528e639397172e2724c970e03fad634719
doYouSpeakFish <ed_sills@hotmail.co.uk>
2021-01-26 10:24:03 +0000

    Began adding getters and setters

--- quick-monster/src/app/monster-action.ts
+++ quick-monster/src/app/monster-action.ts
@@ -5,19 +5,47 @@ export class MonsterAction {
     name: string = "";
     text: string = "";
 
-    limitedUse: boolean = false;
-    multiTarget: boolean = false;
-    multiAttack: number = 0;
+    private _limitedUse: boolean = false;
+    private _multiTarget: boolean = false;
+    public get multiTarget(): boolean {
+        return this._multiTarget;
+    }
+    public set multiTarget(value: boolean) {
+        this._multiTarget = value;
+    }
+    _multiAttack: number = 0;
 
-    attack: boolean = true;
+    _attack: boolean = true;
     attackText: string = "";
-    attackDamage: number = 50;
+    _attackDamage: number = 50;
 
-    save: boolean =  false;
+    _save: boolean =  false;
     saveText: string = "";
-    saveDamage: number = 50;
+    _saveDamage: number = 50;
     saveType: string = "Con";
 
     constructor(public monster: Monster) {}
+
+    set attackDamage(relativeDamage: number) {
+        this._attackDamage = relativeDamage;
+        // TODO calculate dice and modifiers
+    }
+    get attackDamage() {
+        return this._attackDamage;
+    }
+
+    set saveDamage(relativeDamage: number) {
+        this._saveDamage = relativeDamage;
+    }
+    get saveDamage() {
+        return this._saveDamage;
+    }
+
+    public get limitedUse(): boolean {
+        return this._limitedUse;
+    }
+    public set limitedUse(value: boolean) {
+        this._limitedUse = value;
+    }
 
 }
